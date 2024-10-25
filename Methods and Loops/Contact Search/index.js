d9a6af08eed04d681e67c12761f030399dbd4746
[Darrick Fauvel] <darrick@darrickdevelops.com>
2024-10-25 17:20:22 -0400

    setup real estate challenge

--- Methods and Loops/Contact Search/index.js
+++ Methods and Loops/Contact Search/index.js
@@ -6,36 +6,35 @@ const contactDisplay = document.getElementById("contact-display")
 
 function renderContact(contactObj) {
   const { name, email, phone } = contactObj
-
   const contactCard = document.createElement("aside")
   contactCard.classList.add("contact-card")
-
   const p1 = document.createElement("p")
   p1.textContent = name
   contactCard.appendChild(p1)
-
   const p2 = document.createElement("p")
   p2.textContent = email
   contactCard.appendChild(p2)
-
   const p3 = document.createElement("p")
   p3.textContent = phone
   contactCard.appendChild(p3)
-
   contactDisplay.appendChild(contactCard)
+}
+
+function getFilteredContacts(searchTerm) {
+  return contactsArr.filter((contact) => {
+    return Object.values(contact).some((value) =>
+      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+    )
+  })
 }
 
 function handleSubmit(e) {
   e.preventDefault()
   contactDisplay.innerHTML = ""
   const searchTerm = patternSearchInput.value
-  const foundContacts = contactsArr.filter((contact) => {
-    return Object.values(contact).some((value) =>
-      value.toString().toLowerCase().includes(searchTerm.toLowerCase())
-    )
-  })
-  if (foundContacts.length > 0) {
-    foundContacts.forEach((contact) => renderContact(contact))
+  const filteredContacts = getFilteredContacts(searchTerm)
+  if (filteredContacts.length > 0) {
+    filteredContacts.forEach((contact) => renderContact(contact))
     patternSearchInput.value = ""
     patternSearchInput.focus()
   }
